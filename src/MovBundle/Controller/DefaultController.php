dfa8a59bceb918c2348d9fe2dd2a761506f4fe8a
Julien <j.saintlaurent44@gmail.com>
2016-11-10 02:55:46 +0100

    creation des mails

--- src/MovBundle/Controller/DefaultController.php
+++ src/MovBundle/Controller/DefaultController.php
@@ -3,10 +3,25 @@
 namespace MovBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function sendMailAction()
     {
+        $Request = $this->getRequest();
+        if ($Request->getMethod() == "POST") {
+            $Subject = $Request->get("Subject");
+            $message = " " . $Request->get("message");
+            $message = \Swift_Message::newInstance('Test')
+                ->setSubject($Subject)
+                ->setFrom('laposte974@laposte.net')
+                ->setTo('laposte974@laposte.net')
+                ->setBody($message);
+            $this->get('mailer')->send($message);
+            return $this->render('MovBundle:Default:mail.html.twig');
+        }
+        return $this->render('MovBundle:Default:formMail.html.twig');
     }
+
 }
